0337f73b909950f7e51138e19d700c5221351495
root <spmassot@gmail.com>
2018-01-05 23:56:29 -0500

    Updates auth so it actually works

--- authorization.py
+++ authorization.py
@@ -1,4 +1,4 @@
-import hmac, time, base64
+import hmac, time, base64, hashlib
 from requests.auth import AuthBase
 from os import environ as env
 
@@ -11,21 +11,20 @@ class CoinbaseExchangeAuth(AuthBase):
 
     def __call__(self, request):
         timestamp = str(time.time())
-        message = bytes(
+        message = (
             timestamp +
             request.method +
             request.path_url +
-            (request.body or ''),
-            'latin-1'
+            (request.body or '')
         )
+        message = message.encode('ascii')
         hmac_key = base64.b64decode(self.secret_key)
-        signature = hmac.new(hmac_key, message)
-        signature_b64 = base64.b64encode(
-            bytes(signature.hexdigest(), 'latin-1')
-        )
+        assert len(hmac_key) == 64
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest())
 
         request.headers.update({
-            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-SIGN': signature_b64.decode('ascii'),
             'CB-ACCESS-TIMESTAMP': timestamp,
             'CB-ACCESS-KEY': self.api_key,
             'CB-ACCESS-PASSPHRASE': self.passphrase,
--- get_accounts.py
+++ get_accounts.py
@@ -1,11 +1,14 @@
-import pytest
-import requests
 from authorization import CoinbaseExchangeAuth
+import requests
 
 
-def test_CoinbaseExchangeAuth():
+def get_accounts():
     url = 'https://api.gdax.com/accounts'
     auth = CoinbaseExchangeAuth()
     response = requests.get(url, auth=auth)
-    assert response.status_code == 200
+    return response.json()
+
+if __name__ == '__main__':
+    for x in get_accounts():
+        print(x)
 
