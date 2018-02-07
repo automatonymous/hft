0f6e2929e2e15136e52a2f5187f061ff57459332
spmassot <sean.massot@namely.com>
2018-02-06 21:48:11 -0500

    pyuups

--- sync_src/channels.py
+++ sync_src/channels.py
@@ -1,6 +1,6 @@
 from json import dumps, loads
 from os import environ as env
-import websockets
+from websocket import create_connection
 
 from src.authorization import get_auth_dict
 from utils.logger import log
@@ -18,33 +18,26 @@ class Channel:
             "channels": self.channels,
         })
 
-    async def listen(self):
+    def listen(self):
         log(f'{self.channels} --- Initializing connection')
         cycles = 0
         try:
             self.alive = True
-            async with websockets.connect(
-                env['WEBSOCKET_URL'],
-                max_queue=0,
-            ) as websocket:
-                websocket.send(dumps(self.sub_message))
-                async for message in websocket:
-                    if not self.alive: break
-                    self.queue.push(loads(message))
-                    cycles += 1
-                    if cycles % 300 == 0:
-                        websocket.ping('keepalive')
-                        log(
-                            f'{self.channels} --- Still alive asof {cycles}'
-                        )
+            websocket = create_connection(env['WEBSOCKET_URL'])
+            websocket.send(dumps(self.sub_message))
+            while self.alive:
+                data = websocket.recv()
+                self.queue.push(loads(data))
+                cycles += 1
+                if cycles % 300 == 0:
+                    websocket.ping('keepalive')
+                    log(
+                        f'{self.channels} --- Still alive asof {cycles}'
+                    )
         except Exception as e:
             log(f'{self.channels} --- {e}', level='error')
 
-    async def stop(self):
+    def stop(self):
         log(f'{self.channels} --- Destroying connection')
         self.alive = False
-
-
-if __name__ == '__main__':
-    from src.data_structures import L2Queue
 
